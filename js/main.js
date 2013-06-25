422c9b9a0d70281fecf126cbdcca11c94bf2805c
Jeffrey Kao <jeffthestampede@gmail.com>
2013-06-25 15:28:38 -0700

    cleaned up js and added links to carousel

--- js/main.js
+++ js/main.js
@@ -1,20 +1,11 @@
-$(function(){
-  $("#slides").slidesjs({
-    width: 740,
-    height: 328
-  });
-});
-
 $(document).ready(function() {
 
+  $("#slides").slidesjs();
   $('.slidesjs-previous ').html('<i class="icon-chevron-left icon-large"></i>').css({'color': 'black', 'text-decoration': 'none'});
   $('.slidesjs-next ').html('<i class="icon-chevron-right icon-large"></i>').css({'color': 'black', 'text-decoration': 'none'});
-
-
-});
-
-$(document).ready(function() {
   $('.menu').localScroll();
+
 });
+
 
 
